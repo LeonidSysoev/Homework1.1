42536d21a00514ca0ad8682ae144d5f9c6518017
LeonidHome <defenser2205@gmail.com>
2023-01-03 13:04:00 +0300

    task4,task5

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
-        task3();}
+        task3();
+        task4();
+        task5();}
 
     public static void task1 () {
         System.out.println("Задача 1");
@@ -36,5 +38,25 @@ public class Main {
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
+    }
+    public static void task4 () {
+        System.out.println("Задача 4");
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend * 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+    }
+    public static void task5 () {
+        System.out.println("Задача 5");
+        var frog = 3.5;
+        System.out.println(frog);
+        frog *= 10 ;
+        System.out.println(frog);
+        frog /= 3.5;
+        System.out.println(frog);
+        frog += 4;
+        System.out.println(frog);
     }
 }
